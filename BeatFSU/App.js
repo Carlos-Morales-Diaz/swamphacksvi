79443fa6b0ec1943ff5ec54ef920989548287267
AnthonyBower <anthonybower6@yahoo.com>
2020-02-02 02:21:12 -0500

    Hammer rotates, counter works. Needs formatting and timer.

--- BeatFSU/App.js
+++ BeatFSU/App.js
@@ -8,13 +8,17 @@ import {
   Button,
   TouchableOpacity,
   View,
+  Animated,
+  Easing,
 } from 'react-native';
 
 export default class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      spinValue: new Animated.Value(0),
+      ongoingAnimation: false
     }
   }
 
@@ -26,9 +30,51 @@ export default class Counter extends React.Component {
           <Text>{this.state.counter}</Text>
         </Text>
 
-        <TouchableOpacity onPress = { () => { this.setState( (prevState) => {return { counter: prevState.counter + 1} } )}} >
+        {
+          !(this.state.ongoingAnimation) ?
+          <TouchableOpacity onPress = { () => { 
+                                                this.setState( 
+                                                      (prevState) => {
+                                                          return { counter: prevState.counter + 1,
+                                                                   ongoingAnimation: true } 
+                                                                  } );
+                                                
+                                                Animated.timing(
+                                                  this.state.spinValue,
+                                                  {
+                                                    toValue: 1,
+                                                    duration: 3000,
+                                                    easing: Easing.linear,
+                                                    useNativeDriver: true
+                                                  }
+                                                ).start( () => {
+                                                  Animated.timing(
+                                                    this.state.spinValue,
+                                                    {
+                                                      toValue: 0,
+                                                      duration: 3000,
+                                                      easing: Easing.linear,
+                                                      useNativeDriver: true
+                                                    }
+                                                  ).start( () => 
+                                                          { this.setState( {ongoingAnimation: false} ) } );
+                                                });
+                                              }} >
+            <Image source={require('./Images/fsu.png')} style={styles.BigButton}/>
+          </TouchableOpacity>
+          :
           <Image source={require('./Images/fsu.png')} style={styles.BigButton}/>
-        </TouchableOpacity>
+        }
+
+        <Animated.Image 
+            style = { { transform: [{rotate: this.state.spinValue.interpolate({
+                                                                                inputRange: [0, 1],
+                                                                                outputRange: ['0deg', '-90deg']
+                                                                              })
+                                    }]
+                    } }
+            source={require('./Images/hammer.jpg')} />
+
       </View>
     );
   }
@@ -52,5 +98,8 @@ const styles = StyleSheet.create({
   CounterContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
+  },
+  Tomahawk: {
+    alignItems: 'center'
   }
 });
