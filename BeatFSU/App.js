7d55c43646a0fa7786d5e2ba308b93d4e19fac41
Carlos-Morales-Diaz <cmoralesdiaz@ufl.edu>
2020-02-02 04:40:42 -0500

    fixed image sizes

--- BeatFSU/App.js
+++ BeatFSU/App.js
@@ -24,58 +24,70 @@ export default class Counter extends React.Component {
 
   render() {
     return (
-      <View style = {styles.Container}>
-        <Text style = {styles.CounterStyle}>
+      <View style={styles.Container}>
+        <Text style={styles.CounterStyle}>
           <Text>Whacks: </Text>
           <Text>{this.state.counter}</Text>
         </Text>
 
         {
           !(this.state.ongoingAnimation) ?
-          <TouchableOpacity onPress = { () => { 
-                                                this.setState( 
-                                                      (prevState) => {
-                                                          return { counter: prevState.counter + 1,
-                                                                   ongoingAnimation: true } 
-                                                                  } );
-                                                
-                                                Animated.timing(
-                                                  this.state.spinValue,
-                                                  {
-                                                    toValue: 1,
-                                                    duration: 20,
-                                                    easing: Easing.linear,
-                                                    useNativeDriver: true
-                                                  }
-                                                ).start( () => {
-                                                  Animated.timing(
-                                                    this.state.spinValue,
-                                                    {
-                                                      toValue: 0,
-                                                      duration: 20,
-                                                      easing: Easing.linear,
-                                                      useNativeDriver: true
-                                                    }
-                                                  ).start( () => 
-                                                          { this.setState( {ongoingAnimation: false} ) } );
-                                                });
-                                              }} >
-            <Image source={require('./Images/fsu.png')} style={styles.BigButton}/>
-          </TouchableOpacity>
-          :
-          <Image source={require('./Images/fsu.png')} style={styles.BigButton}/>
+            <TouchableOpacity onPress={() => {
+              this.setState(
+                (prevState) => {
+                  return {
+                    counter: prevState.counter + 1,
+                    ongoingAnimation: true
+                  }
+                });
+
+              Animated.timing(
+                this.state.spinValue,
+                {
+                  toValue: 1,
+                  duration: 100,
+                  easing: Easing.linear,
+                  useNativeDriver: true
+                }
+              ).start(() => {
+                Animated.timing(
+                  this.state.spinValue,
+                  {
+                    toValue: 0,
+                    duration: 100,
+                    easing: Easing.linear,
+                    useNativeDriver: true
+                  }
+                ).start(() => { this.setState({ ongoingAnimation: false }) });
+              });
+            }} >
+              <Image 
+              
+              source={require('./Images/fsu.png')} style={styles.BigButton} />
+            </TouchableOpacity>
+            :
+            <Image source={require('./Images/fsu.png')} style={styles.BigButton} />
         }
 
-        <Animated.Image 
-            style = { { transform: [{rotate: this.state.spinValue.interpolate({
-                                                                                inputRange: [0, 1],
-                                                                                outputRange: ['0deg', '-90deg']
-                                                                              })
-                                    }]
-                    } }
-            source={require('./Images/hammer.jpg')} />
 
-      </View>
+          <Animated.Image
+            style={
+              {
+                marginLeft: 80,
+                marginTop: -350,
+                width: 150.95652174,
+                height: 200,
+                resizeMode: 'stretch',
+                transform: [{
+                  rotate: this.state.spinValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ['0deg', '90deg']
+                  })
+                }]
+              }}
+            source={require('./Images/tomahawk.png')} />
+        </View>
+            
     );
   }
 }
@@ -92,14 +104,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   BigButton: {
-    alignItems: 'center',
+    marginLeft: 100,
     marginTop: 200,
   },
   CounterContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
   },
-  Tomahawk: {
-    alignItems: 'center'
-  }
 });
